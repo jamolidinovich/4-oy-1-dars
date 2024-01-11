8aaba517c8f23aa762515139c376ac76d327bd1b
anazirjonov <anazirjonov10@gmail.com>
2024-01-11 15:18:03 +0500

    new

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// 1-misol.
+// let firstNumber = +prompt("Kunbi enini kiriting");
+// let secondNumber = +prompt("Kubni buyi kiriting ");
+// let heightNuber = +prompt("Kubni balanligi kiriting ");
+// let volume = firstNumber * secondNumber * heightNuber;
+// console.log(volume);
+
+// 2-misol.
+// let Number = +prompt("Soni kiriting");
+// let answer = Number * Number;
+// console.log(answer);
+
+// 3-misol.
+// let Number1 = +prompt("birinchi soni kiriting");
+// let Number2 = +prompt("ikkinchi soni kiriting");
+// let NumberAnswer = Number1 + Number2;
+// let qoldiq = NumberAnswer * 3;
+// console.log(qoldiq);
+
+// 4-misol.
+
+// let arbitraryNumber = +prompt("Ixtiyori soningizni kiriting");
+// let arbitraryNumber1 = arbitraryNumber % 10;
+// let arbitraryNumber2 = arbitraryNumber / 10;
+// console.log(arbitraryNumber2);
+
+//5-misol
+// let firstnumber = +prompt("ikki xonali soni kiriting");
+
+// let firstnumberAnswer = firstnumber % 10;
+// let firstnumberAnswer1 = (firstnumber / 10) % 10;
+// let firstnumberAnswerres = firstnumberAnswer + firstnumberAnswer1;
+// console.log(firstnumberAnswerres);
+
+// 6-misol.
+
+// let number6 = +prompt("uch xonali son kiriting");
+// let number6Answer = (number6 / 100) % 10;
+// console.log(number6Answer);
+
+// 7-misol.
+
+// let paralopiped = +prompt("balandlikni  kiriting");
+// let paralopipedw = +prompt("widhtsini kiriting");
+// let paralopipedh = +prompt("height");
+// let hajm = +prompt("hajmini kiriting");
+// let javob = paralopiped + paralopipedh + paralopipedw * hajm * hajm;
+// console.log(javob);
+
+// 8-misol.
+
+let son = +prompt("soni kiriting");
+let key = (son = 1 <= 10);
+console.log(key);
